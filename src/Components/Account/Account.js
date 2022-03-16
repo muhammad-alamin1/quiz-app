320ac321f014b75b548cfd4dd31a4fca7e00c290
Muhammad <muhammad@kali.kali>
2022-03-16 07:57:47 +0600

    add signin & signout func

--- src/Components/Account/Account.js
+++ src/Components/Account/Account.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../../contexts/AuthContext';
 import './account.css';
-
 export default function Account() {
+    const { currUser, logout } = useAuth();
     return (
         <div class="account">
-            <span class="material-icons-outlined" title="Account">
-                account_circle
-            </span>
-            <Link to="/signup">Signup</Link>
-            <Link to="/login">Login</Link>
-            <span class="material-icons-outlined" title="Logout"> logout </span>
+            {
+                currUser ? (
+                    <>
+                        <span class="material-icons-outlined" title="Account">
+                            account_circle
+                        </span>
+                        <span>{currUser.displayName}</span>
+                        <span class="material-icons-outlined" title="Logout" onClick={logout} > logout </span>
+                    </>
+                ) : (
+                    <>
+                        <Link to="/signup">Signup</Link>
+                        <Link to="/login">Login</Link>
+                    </>
+                )
+            }
         </div>
     )
 }
--- src/Components/Pages/Login/Login.js
+++ src/Components/Pages/Login/Login.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../../contexts/AuthContext';
 import './login.css';
 
 export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [error, setError] = useState();
+    const [loading, setLoading] = useState();
+
+    const { login } = useAuth();
+    let history = useNavigate();
+
+    // submit the form
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            setError("");
+            setLoading(true);
+
+            await login(email, password);
+            history('/');
+
+        } catch (error) {
+            console.log(error);
+            setLoading(false);
+            setError(`Authentication Failed.!`);
+        }
+
+    }
+
     return (
         <>
             <div className="column">
                 <h3>Sign In Your Account</h3> <br />
-                <form className="login form" action="#">
+                <form className="login form" action="#" onSubmit={handleSubmit} >
                     <div className="textInput">
-                        <input type="text" placeholder="E-mail" />
+                        <input type="text" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} required />
                         <span className="material-icons-outlined"> alternate_email </span>
                     </div> <br />
 
                     <div className="textInput">
-                        <input type="password" placeholder="Password" />
+                        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
                         <span className="material-icons-outlined"> lock </span>
                     </div> <br />
 
-                    <button className="button">
+                    {
+                        error && <p className="error">{error}</p>
+                    }
+
+                    <button disabled={loading} className="button" type="submit" >
                         <span>Sign In</span>
                     </button>
 
